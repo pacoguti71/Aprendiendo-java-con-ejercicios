a64b921c5d47452de6b4774eee8a8257f4d60072
pacoguti71 <pacoguti71@gmail.com>
2025-07-01 20:06:24 +0200

    Segunda grabación finalizada

---
+++ 01_Salida_de_datos_por_pantalla/src/principal/S01Ejercicio09.java
@@ -0,0 +1,34 @@
+package principal;
+
+/**
+ * Escribe un programa que pinte por pantalla alguna escena - el campo, la
+ * habitación de una casa, un aula, etc. - o algún objeto animado o inanimado -
+ * un coche, un gato, una taza de café, etc. Ten en cuenta que puedes utilizar
+ * caracteres como *, +, <, #, @, etc. o incluso caracteres Unicode. ¡Échale
+ * imaginación!
+ *
+ */
+public class S01Ejercicio09 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" | \u265C | \u265E | \u265D | \u265B | \u265A | \u265D | \u265E | \u265C |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" | \u265F | \u265F | \u265F | \u265F | \u265F | \u265F | \u265F | \u265F |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" |   |   |   |   |   |   |   |   |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" |   |   |   |   |   |   |   |   |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" |   |   |   |   |   |   |   |   |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" |   |   |   |   |   |   |   |   |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" | \u265F | \u265F | \u265F | \u265F | \u265F | \u265F | \u265F | \u265F |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+        System.out.println(" | \u265C | \u265E | \u265D | \u265B | \u265A | \u265D | \u265E | \u265C |");
+        System.out.println(" +-──-+-──-+-──-+-──-+-──-+-──-+-──-+-──-+");
+    } // Fin main
+
+} // Fin class
