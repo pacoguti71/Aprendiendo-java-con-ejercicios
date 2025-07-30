365ef8cfa873c48664cc9cd7169a13715aeb13fe
pacoguti <pacoguti@Principal>
2025-07-30 16:58:03 +0200

    Tema 4. Ejercicio 04

---
+++ 04_Sentencia_condicional/src/principal/S04Ejercicio04.java
@@ -0,0 +1,32 @@
+package principal;
+
+import java.util.Scanner;
+
+public class S04Ejercicio04 {
+
+    public static void main(String[] args) {
+        final int COSTE_HORA = 12;
+        final int COSTE_HORA_EXTRA = 16;
+        final int MAX_HORAS_NORMALES = 40;
+
+        int numHoras;
+        int numHorasExtra;
+        int salario;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Por favor, intruzca el número de horas trabajadas durante la semana: ");
+        numHoras = teclado.nextInt();
+
+        if (numHoras <= MAX_HORAS_NORMALES) {
+            salario = numHoras * COSTE_HORA;
+        } else {
+            numHorasExtra = numHoras - MAX_HORAS_NORMALES;
+            salario = (MAX_HORAS_NORMALES * COSTE_HORA) + (numHorasExtra * COSTE_HORA_EXTRA);
+        }
+
+        System.out.printf("El sueldo semanal que le corresponde es de %d euros.\n", salario);
+
+    } // Fin main
+
+} // Fin class
